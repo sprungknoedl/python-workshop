a9104400aab4e0e747ea6a51e4202f5402e020b2
Thomas Kastner <tk@sprungknoedl.at>
2010-12-20 15:37:46 +0100

    ircbot code, untested!

---
+++ src2/ircbot.py
@@ -0,0 +1,56 @@
+from twisted.words.protocols import irc
+from twisted.internet import reactor, protocol
+
+class Bot(irc.IRCClient):
+	'''
+	harvesting irc bot
+	'''
+	
+	nickname = 'harvest-moon'
+	channel = '#hackinggroup'
+	
+	def signedOn(self):
+		'''
+		called when bot has succesfully signed on to server.
+		'''
+		
+		self.join(channel)
+		
+	def privmsg(self, user, channel, msg):
+		'''
+		this will get called when the bot receives a message. it doesn't matter 
+		if this is a message in a channel or a private message
+		'''
+		
+		if msg.startswith('!start '):
+			# do action
+			self.msg(channel, '[starting to harvest]')
+			
+class Factory(protocol.ClientFactory):
+	'''
+	factory for harvester bots. a new protocol instance will be created each
+	time we connect to the server (in our example once).
+	'''
+	
+	# the class of the protocol to build when new connection is made
+	protocol = Bot
+	
+	def clientConnectionLost(self, connector, reason):
+		'''
+		called when tcp connection is lost => trying to reconnect
+		'''
+		
+		print "connection lost: %s" % reason
+		connector.connect()
+		
+	def clientConnectionFailed(self, connector, reason):
+		'''
+		called when tcp connection attemp failed!
+		'''
+		
+		print "connection failed: %s" % reason
+		reactor.stop()
+		
+if __name__ == '__main__':
+	reactor.connectTCP('irc.freenode.net', 6667, Factory())
+	reactor.run()
